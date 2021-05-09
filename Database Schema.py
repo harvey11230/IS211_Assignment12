912856182e79f8745858c2dbd337398da602f034
harvey11230 <31376335+harvey11230@users.noreply.github.com>
2021-05-09 02:14:53 -0400

    Add files via upload

--- Database Schema.py
+++ Database Schema.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-def create_sutdents_table():
-    with sqlite3.connect("Students.db") as s:
-        c = s.cursor()
+def main():
+    with sqlite3.connect("database.db") as d:
 
+        c = d.cursor()
         c.execute("""
         CREATE TABLE IF NOT EXISTS students(
         student_id INTEGER PRIMARY KEY,
         firstname TEXT,
-        lastname TEXT);
+        lastname TEXT)
         """)
 
         c.execute("""
@@ -16,12 +16,6 @@ def create_sutdents_table():
         VALUES (1, 'Wei', 'Huang');
         """)
 
-        s.commit()
-
-
-def create_quizzes_table():
-    with sqlite3.connect("Quizzes.db") as q:
-        c = q.cursor()
         c.execute("""
         CREATE TABLE IF NOT EXISTS quizzes(
         quiz_id INTEGER PRIMARY KEY,
@@ -32,20 +26,13 @@ def create_quizzes_table():
         INSERT INTO quizzes (quiz_id, subject)
         VALUES (1, 'Python');
         """)
-        q.commit()
 
-
-def create_result_table():
-    with sqlite3.connect("Results.db") as r:
-        c = r.cursor()
         c.execute("""
         CREATE TABLE IF NOT EXISTS results(
         result_id INTEGER PRIMARY KEY,
         student_id INTEGER,
         quiz_id INTEGER,
-        score INTEGER,
-        FOREIGN KEY (student_id) REFERENCES students (student_id),
-        FOREIGN KEY (quiz_id) REFERENCES quizzes (quiz_id))
+        score INTEGER)
         """)
 
         c.execute("""
@@ -53,13 +40,7 @@ def create_result_table():
         VALUES (1, 1, 1, 100);
         """)
 
-        r.commit()
-
-
-def main():
-    create_sutdents_table()
-    create_quizzes_table()
-    create_result_table()
+        d.commit()
 
 if __name__ == '__main__':
     main()
--- Records.py
+++ Records.py
@@ -1,10 +1,22 @@
 import sqlite3
+from flask import Flask, render_template
 
-conn = sqlite3.connect("Results.db")
+conn = sqlite3.connect("database.db")
 
 c = conn.cursor()
 
-c.execute("SELECT * FROM results INNER JOIN students USING (student_id)"
-          "INNER JOIN quizzes USING (quiz_id)")
+c.execute("""SELECT students.student_id, firstname, lastname, subject, score FROM results 
+            INNER JOIN students USING (student_id)
+            INNER JOIN quizzes USING (quiz_id)""")
 
-print(c.fetchall())
+result = c.fetchall()
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    return render_template('dashboard.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
